09e83ce8ee8a5c03206dc3a359006267b96c0b21
Ernest Guevarra <ernest@guevarra.io>
2021-03-06 20:07:16 +0000

    label lists

--- R/sds011.R
+++ R/sds011.R
@@ -100,7 +100,13 @@ sds011_read <- function(con = sds011_connect(), n = 0) {
 
 sds011_read_int <- function(con = sds011_connect(), n = 0, interval = 5) {
   while (TRUE) {
-    print(list(Sys.time(), sds011_read(con = con, n = 0)))
+    print(
+      list(
+        date_time = Sys.time(),
+        reading = sds011_read(con = con, n = 0)
+      )
+    )
+
     Sys.sleep(time = interval)
   }
 }
